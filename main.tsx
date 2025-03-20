9911def3bef5217640efa08249b043cdfe803985
taisan11 <131413638+taisan11@users.noreply.github.com>
2025-03-20 19:13:13 +0900

    a

--- main.tsx
+++ main.tsx
@@ -1,7 +1,11 @@
 import { Hono } from 'hono'
 import {jsxRenderer} from "hono/jsx-renderer"
+import type {MiddlewareHandler} from "hono"
 
 const app = new Hono()
+
+//Plugin Type
+type Plugin = () => {HonoMiddleware: MiddlewareHandler}
 
 app.use(jsxRenderer(({ children }) => {
   return (
